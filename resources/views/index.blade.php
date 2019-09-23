0b3fb8cfbfa0f4d75ab6e5464b6c85df1bbfac54
unknown <C:\Users\FIG\AppData\Roaming\The Bat!>
2019-09-23 21:12:13 +0300

    Final Blade make + Route make

---
+++ resources/views/index.blade.php
@@ -0,0 +1,380 @@
+@extends('2_layout')
+
+@section('karysel')
+  <!-- ##### Hero Area Start ##### -->
+<section class="hero-area">
+    <div class="hero-post-slides owl-carousel">
+
+        <!-- Single Hero Post -->
+        <div class="single-hero-post">
+            <!-- Post Image -->
+            <div class="slide-img bg-img" style="background-image: url(img/blog-img/2.jpg);"></div>
+            <!-- Post Content -->
+            <div class="hero-slides-content">
+                <p>MAY 8, 2018 / 35 Comments</p>
+                <a href="#" class="post-title">
+                    <h4>Fried Pizzas on parchment paper with basil.</h4>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Hero Post -->
+        <div class="single-hero-post">
+            <!-- Post Image -->
+            <div class="slide-img bg-img" style="background-image: url(img/blog-img/3.jpg);"></div>
+            <!-- Post Content -->
+            <div class="hero-slides-content">
+                <p>MAY 8, 2018 / Foody</p>
+                <a href="#" class="post-title">
+                    <h4>Quick Vegan Enchiladas with Sweet Potato Sauce</h4>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Hero Post -->
+        <div class="single-hero-post">
+            <!-- Post Image -->
+            <div class="slide-img bg-img" style="background-image: url(img/blog-img/4.jpg);"></div>
+            <!-- Post Content -->
+            <div class="hero-slides-content">
+                <p>aug 8, 2018 / health</p>
+                <a href="#" class="post-title">
+                    <h4>Eleven Slushie Cocktails I Bookmarked</h4>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Hero Post -->
+        <div class="single-hero-post">
+            <!-- Post Image -->
+            <div class="slide-img bg-img" style="background-image: url(img/blog-img/5.jpg);"></div>
+            <!-- Post Content -->
+            <div class="hero-slides-content">
+                <p>MAY 8, 2018 / drinks</p>
+                <a href="#" class="post-title">
+                    <h4>Homemade Strawberry Almond Milk</h4>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Hero Post -->
+        <div class="single-hero-post">
+            <!-- Post Image -->
+            <div class="slide-img bg-img" style="background-image: url(img/blog-img/6.jpg);"></div>
+            <!-- Post Content -->
+            <div class="hero-slides-content">
+                <p>MAY 8, 2018 / foody</p>
+                <a href="#" class="post-title">
+                    <h4>Kitchen Sink Sunflower Salad in a bow.</h4>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Hero Post -->
+        <div class="single-hero-post">
+            <!-- Post Image -->
+            <div class="slide-img bg-img" style="background-image: url(img/blog-img/2.jpg);"></div>
+            <!-- Post Content -->
+            <div class="hero-slides-content">
+                <p>MAY 8, 2018 / 35 Comments</p>
+                <a href="#" class="post-title">
+                    <h4>Fried Pizzas on parchment paper with basil.</h4>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Hero Post -->
+        <div class="single-hero-post">
+            <!-- Post Image -->
+            <div class="slide-img bg-img" style="background-image: url(img/blog-img/3.jpg);"></div>
+            <!-- Post Content -->
+            <div class="hero-slides-content">
+                <p>MAY 8, 2018 / Foody</p>
+                <a href="#" class="post-title">
+                    <h4>Quick Vegan Enchiladas with Sweet Potato Sauce</h4>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Hero Post -->
+        <div class="single-hero-post">
+            <!-- Post Image -->
+            <div class="slide-img bg-img" style="background-image: url(img/blog-img/4.jpg);"></div>
+            <!-- Post Content -->
+            <div class="hero-slides-content">
+                <p>aug 8, 2018 / health</p>
+                <a href="#" class="post-title">
+                    <h4>Eleven Slushie Cocktails I Bookmarked</h4>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Hero Post -->
+        <div class="single-hero-post">
+            <!-- Post Image -->
+            <div class="slide-img bg-img" style="background-image: url(img/blog-img/5.jpg);"></div>
+            <!-- Post Content -->
+            <div class="hero-slides-content">
+                <p>MAY 8, 2018 / drinks</p>
+                <a href="#" class="post-title">
+                    <h4>Homemade Strawberry Almond Milk</h4>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Hero Post -->
+        <div class="single-hero-post">
+            <!-- Post Image -->
+            <div class="slide-img bg-img" style="background-image: url(img/blog-img/6.jpg);"></div>
+            <!-- Post Content -->
+            <div class="hero-slides-content">
+                <p>MAY 8, 2018 / foody</p>
+                <a href="#" class="post-title">
+                    <h4>Kitchen Sink Sunflower Salad in a bow.</h4>
+                </a>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- ##### Hero Area End ##### -->
+@endsection
+
+@section('body')
+<!-- ##### Blog Content Area Start ##### -->
+
+        <div class="row">
+            <!-- Featured Post Area -->
+            <div class="col-12">
+                <div class="featured-post-area">
+                    <div id="featured-post-slides" class="carousel slide d-flex flex-wrap" data-ride="carousel">
+
+                        <div class="carousel-inner">
+
+                            <div class="carousel-item active bg-img" style="background-image: url(img/blog-img/7.jpg)">
+                                <!-- Featured Post Content -->
+                                <div class="featured-post-content">
+                                    <p>MAY 8, 2018 / foody</p>
+                                    <a href="#" class="post-title">
+                                        <h2>A Closer Look At Our Front Porch Items From Lowe’s</h2>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item bg-img" style="background-image: url(img/blog-img/6.jpg)">
+                                <!-- Featured Post Content -->
+                                <div class="featured-post-content">
+                                    <p>MAY 8, 2018 / foody</p>
+                                    <a href="#" class="post-title">
+                                        <h2>12 Beautiful Vegan Panna Cottas</h2>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item bg-img" style="background-image: url(img/blog-img/2.jpg)">
+                                <!-- Featured Post Content -->
+                                <div class="featured-post-content">
+                                    <p>MAY 8, 2018 / foody</p>
+                                    <a href="#" class="post-title">
+                                        <h2>Quick Vegan Enchiladas with Sweet Potato Sauce</h2>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item bg-img" style="background-image: url(img/blog-img/3.jpg)">
+                                <!-- Featured Post Content -->
+                                <div class="featured-post-content">
+                                    <p>MAY 8, 2018 / foody</p>
+                                    <a href="#" class="post-title">
+                                        <h2>Instant Pot Mushroom Stroganoff with Vodka</h2>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item bg-img" style="background-image: url(img/blog-img/5.jpg)">
+                                <!-- Featured Post Content -->
+                                <div class="featured-post-content">
+                                    <p>MAY 8, 2018 / foody</p>
+                                    <a href="#" class="post-title">
+                                        <h2>The Most Popular Recipe Last Month</h2>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <ol class="carousel-indicators">
+                            <li data-target="#featured-post-slides" data-slide-to="0" class="active">
+                                <h2>01</h2>
+                                <a href="#" class="post-title">
+                                    <h5>A Closer Look At Our Front Porch Items From Lowe’s</h5>
+                                </a>
+                            </li>
+                            <li data-target="#featured-post-slides" data-slide-to="1">
+                                <h2>02</h2>
+                                <a href="#" class="post-title">
+                                    <h5>12 Beautiful Vegan Panna Cottas</h5>
+                                </a>
+                            </li>
+                            <li data-target="#featured-post-slides" data-slide-to="2">
+                                <h2>03</h2>
+                                <a href="#" class="post-title">
+                                    <h5>Quick Vegan Enchiladas with Sweet Potato Sauce</h5>
+                                </a>
+                            </li>
+                            <li data-target="#featured-post-slides" data-slide-to="3">
+                                <h2>04</h2>
+                                <a href="#" class="post-title">
+                                    <h5>Instant Pot Mushroom Stroganoff with Vodka</h5>
+                                </a>
+                            </li>
+                            <li data-target="#featured-post-slides" data-slide-to="4">
+                                <h2>05</h2>
+                                <a href="#" class="post-title">
+                                    <h5>The Most Popular Recipe Last Month</h5>
+                                </a>
+                            </li>
+                        </ol>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <!-- Blog Posts Area -->
+            <div class="col-12 col-lg-8">
+                <div class="blog-posts-area">
+
+                    <!-- Single Blog Post -->
+                    <div class="single-blog-post d-flex flex-wrap mt-50">
+                        <!-- Thumbnail -->
+                        <div class="post-thumbnail mb-50">
+                            <a href="#"><img src="img/blog-img/8.jpg" alt=""></a>
+                        </div>
+                        <!-- Content -->
+                        <div class="post-content mb-50">
+                            <p class="post-date">MAY 8, 2018 / foody</p>
+                            <a href="#" class="post-title">
+                                <h4>Vegetarian Spring Pho with Sweet Potato Noodles and Heirloom Beans</h4>
+                            </a>
+                            <div class="post-meta">
+                                <a href="#"><span>by</span> Sarah Jenks</a>
+                                <a href="#"><i class="fa fa-eye"></i> 192</a>
+                                <a href="#"><i class="fa fa-comments"></i> 08</a>
+                            </div>
+                            <p class="post-excerpt">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.</p>
+                            <a href="#" class="read-more-btn">Continue Reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                        </div>
+                        <!-- Post Curve Line -->
+                        <img class="post-curve-line" src="img/core-img/post-curve-line.png" alt="">
+                    </div>
+
+                    <!-- Single Blog Post -->
+                    <div class="single-blog-post d-flex flex-wrap mt-50">
+                        <!-- Thumbnail -->
+                        <div class="post-thumbnail mb-50">
+                            <a href="#"><img src="img/blog-img/9.jpg" alt=""></a>
+                        </div>
+                        <!-- Content -->
+                        <div class="post-content mb-50">
+                            <p class="post-date">MAY 12, 2018 / drinks</p>
+                            <a href="#" class="post-title">
+                                <h4>Grain-Free Sweet &amp; Savory Activated Walnut Granola</h4>
+                            </a>
+                            <div class="post-meta">
+                                <a href="#"><span>by</span> Sarah Jenks</a>
+                                <a href="#"><i class="fa fa-eye"></i> 192</a>
+                                <a href="#"><i class="fa fa-comments"></i> 08</a>
+                            </div>
+                            <p class="post-excerpt">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.</p>
+                            <a href="#" class="read-more-btn">Continue Reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                        </div>
+                        <!-- Post Curve Line -->
+                        <img class="post-curve-line" src="img/core-img/post-curve-line.png" alt="">
+                    </div>
+
+                    <!-- Single Blog Post -->
+                    <div class="single-blog-post d-flex flex-wrap mt-50">
+                        <!-- Thumbnail -->
+                        <div class="post-thumbnail mb-50">
+                            <a href="#"><img src="img/blog-img/10.jpg" alt=""></a>
+                        </div>
+                        <!-- Content -->
+                        <div class="post-content mb-50">
+                            <p class="post-date">MAY 15, 2018 / Coffee</p>
+                            <a href="#" class="post-title">
+                                <h4>Self-Care Interview Series: Gabrielle Russomagno</h4>
+                            </a>
+                            <div class="post-meta">
+                                <a href="#"><span>by</span> Sarah Jenks</a>
+                                <a href="#"><i class="fa fa-eye"></i> 192</a>
+                                <a href="#"><i class="fa fa-comments"></i> 08</a>
+                            </div>
+                            <p class="post-excerpt">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.</p>
+                            <a href="#" class="read-more-btn">Continue Reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                        </div>
+                        <!-- Post Curve Line -->
+                        <img class="post-curve-line" src="img/core-img/post-curve-line.png" alt="">
+                    </div>
+
+                    <!-- Single Blog Post -->
+                    <div class="single-blog-post d-flex flex-wrap mt-50">
+                        <!-- Thumbnail -->
+                        <div class="post-thumbnail mb-50">
+                            <a href="#"><img src="img/blog-img/11.jpg" alt=""></a>
+                        </div>
+                        <!-- Content -->
+                        <div class="post-content mb-50">
+                            <p class="post-date">MAY 20, 2018 / foody</p>
+                            <a href="#" class="post-title">
+                                <h4>Green Skillet Pizza with Asparagus and Pesto</h4>
+                            </a>
+                            <div class="post-meta">
+                                <a href="#"><span>by</span> Sarah Jenks</a>
+                                <a href="#"><i class="fa fa-eye"></i> 192</a>
+                                <a href="#"><i class="fa fa-comments"></i> 08</a>
+                            </div>
+                            <p class="post-excerpt">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.</p>
+                            <a href="#" class="read-more-btn">Continue Reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                        </div>
+                        <!-- Post Curve Line -->
+                        <img class="post-curve-line" src="img/core-img/post-curve-line.png" alt="">
+                    </div>
+
+                    <!-- Single Blog Post -->
+                    <div class="single-blog-post d-flex flex-wrap mt-50">
+                        <!-- Thumbnail -->
+                        <div class="post-thumbnail mb-50">
+                            <a href="#"><img src="img/blog-img/12.jpg" alt=""></a>
+                        </div>
+                        <!-- Content -->
+                        <div class="post-content mb-50">
+                            <p class="post-date">MAY 28, 2018 / health</p>
+                            <a href="#" class="post-title">
+                                <h4>Green Skillet Pizza with Asparagus and Pesto</h4>
+                            </a>
+                            <div class="post-meta">
+                                <a href="#"><span>by</span> Sarah Jenks</a>
+                                <a href="#"><i class="fa fa-eye"></i> 192</a>
+                                <a href="#"><i class="fa fa-comments"></i> 08</a>
+                            </div>
+                            <p class="post-excerpt">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.</p>
+                            <a href="#" class="read-more-btn">Continue Reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                        </div>
+                        <!-- Post Curve Line -->
+                        <img class="post-curve-line" src="img/core-img/post-curve-line.png" alt="">
+                    </div>
+
+                </div>
+
+                <!-- Pager -->
+                <ol class="foode-pager mt-50">
+                    <li><a href="#"><i class="fa fa-long-arrow-left" aria-hidden="true"></i> Newer</a></li>
+                    <li><a href="#">Older <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></li>
+                </ol>
+            </div>
+        {{-- </div> --}}
+   
+<!-- ##### Blog Content Area End ##### -->
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('index');
+});
+
+Route::get('/post', function(){
+    return view ('post');
+});
+
+Route::get('/blog', function(){
+    return view ('blog');
+});
+
+Route::get('/about', function(){
+    return view ('about');
+});
+
+Route::get('/contact', function(){
+    return view ('contact');
+});
+
+Route::get('/typography', function(){
+    return view ('typography');
 });
